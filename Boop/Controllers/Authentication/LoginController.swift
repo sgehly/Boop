041275537e45a89b6b42f5802de07655d61d6454
Sam Gehly <sam@gehly.net>
2017-12-07 21:12:18 -0500

    Init

---
+++ Boop/Controllers/Authentication/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  Boop
+//
+//  Created by Sam Gehly on 11/18/17.
+//  Copyright © 2017 Sam Gehly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- Boop/ViewController.swift
+++ Boop/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
-
+import KeychainAccess;
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for fname in UIFont.familyNames{
+            print(fname)
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let keychain = Keychain(service: "sh.boop.login");
+        let keychainIdentifier: String? = try! keychain.get("identifier");
+        let keychainPassword: String? = try! keychain.get("password");
+        
+        
+        if(keychainIdentifier != nil && keychainPassword != nil){
+            login(identifier: keychainIdentifier, password: keychainPassword)
+            .then{ response -> Void in
+                self.routeTo(identifier: "authenticationNavigation");
+            }
+            .catch { error -> Void in
+                self.showError(title: "Login Error", message: "It seems that we could not log you in with your stored details. Please sign in again.");
+                self.routeTo(identifier: "authenticationNavigation");
+            }
+        }
+
+        
+        self.routeTo(identifier: "authenticationNavigation");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
